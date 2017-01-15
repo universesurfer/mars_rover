407759245ab4d7b4f04ef5c953888ea3c0c7acc7
Nicholas Rodman <gitnicholas@gmail.com>
2017-01-15 22:27:23 +0100

    Iteration Two

--- rover.js
+++ rover.js
@@ -3,8 +3,11 @@ var myRover = {
   direction: 'N',
   roverDirections: ['N', 'E', 'S', 'W'],
   marsGrid: [[0], [0]],
-  obstacles: []
+  obstacles: [],
+  commands: ['f','f', 'f', 'r', 'f', 'f', 'l', 'f', 'f', 'f', 'b', 'b']
 };
+
+
 
 // MOVE FORWARD & BACKWARD COMMANDS
 
@@ -47,49 +50,11 @@ if (command === 'f') {   //FORWARD
 
       }
   }
-
   return myRover.position;
-  // console.log("New Rover Position: " + rover.position[0] + ", " + rover.position[1]);  //Updated Rover Position
+
 }
 
-// WRAPPING THE ROVER
 
-// if (command === 'f') {
-//
-// if ((myRover.position[[0],[0]]) && (myRover.direction === 'S')) {
-//   myRover.position[[10],[0]];
-// }
-//
-// else if ((myRover.position[[0],[0]]) && (myRover.direction === 'W')) {
-//   myRover.position[[0],[10]];
-// }
-//
-// if ((myRover.position[[0],[10]]) && (myRover.direction === 'E')) {
-//   myRover.position[[0],[0]];
-// }
-//
-// else if ((myRover.position[[0],[10]]) && (myRover.direction === 'S')) {
-//   myRover.position[[10],[10]];
-// }
-//
-// if ((myRover.position[[10],[0]]) && (myRover.direction === 'N')) {
-//   myRover.position[[0],[0]];
-// }
-//
-// else if ((myRover.position[[10],[0]]) && (myRover.direction === 'W')) {
-//   myRover.position[[10],[10]];
-// }
-//
-// if ((myRover.position[[10],[10]]) && (myRover.direction === 'N')) {
-//   myRover.position[[0],[10]];
-// }
-//
-// else if ((myRover.position[[0],[0]]) && (myRover.direction === 'E')) {
-//   myRover.position[[10],[0]];
-// }
-//
-//
-// }
 
 // TURN COMMANDS
 
@@ -130,7 +95,78 @@ function turn(command){
     }
 }
   return myRover.direction;
-  // console.log("New Rover Direction: " + myRover.direction);
 
-  // NEED TO THEN ACTUALLY TURN THE ROVER ON THE GRID -
+
+
+
+}
+
+
+// Executing on a Series of Commands From the Commands Array
+
+function commandsArray() {
+
+
+var commandsLength = myRover.commands.length;
+for (var i = 0; i <= commandsLength; i++) {
+        if (myRover.commands[i] === 'f') {
+          move('f');
+        }
+
+        else if (myRover.commands[i] === 'b') {
+          move('b');
+        }
+
+        if (myRover.commands[i] === 'r') {
+          turn('r');
+        }
+        else if (myRover.commands[i] === 'l') {
+          turn('l');
+        }
+
+
+}
+return myRover.position;
+}
+
+
+
+
+// WRAPPING THE ROVER
+
+if (myRover.position[0] === -10) {
+  if (myRover.direction === 'S' && move('f')) {
+      myRover.position[0] = 10;
+  }
+  else if (myRover.direction === 'N' && move('b')) {
+    myRover.position[0] = 10;
+  }
+}
+
+if (myRover.position[0] === 10) {
+  if (myRover.direction === 'N' && move('f')) {
+    myRover.position[0] = -10;
+  }
+  else if (myRover.direction === 'S' && move('b')) {
+  myRover.position[0] = -10;
+  }
+}
+
+if (myRover.position[1] === 10) {
+  if (myRover.direction === 'E' && move('f')) {
+     myRover.position[1] = -10;
+  }
+  else if (myRover.direction == 'W' && move('b')) {
+     myRover.position[1] = -10;
+  }
+}
+
+if (myRover.position[1] === -10) {
+  if (myRover.direction === 'W' && move('f')) {
+    myRover.position[0] = 10;
+  }
+  else if (myRover.direction === 'E' && move('b')) {
+     myRover.position[1] = 10;
+  }
+
 }
